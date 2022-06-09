6b03ea1774ca52980f1e61a567a83c43933e5e25
julian donald <zentixzaneic@gmail.com>
2022-06-09 00:17:33 -0400

    fix position for measuring point
    fix position for the measuring point, and add a comment for what i'm trying to do

--- Assets/Scripts/GraphLocation.cs
+++ Assets/Scripts/GraphLocation.cs
@@ -5,14 +5,30 @@ using UnityEngine;
 
 public class GraphLocation : MonoBehaviour
 {
-    public GameObject graph;
-    public GameObject dot;
+    public GameObject graph, dot, measurePoint;
 
+    // manually set position of game object since unity's and won't let us calc velocity otherwise
+    private void Update()
+    {
+        measurePoint.transform.position = new Vector3(0, 0, 0);
+    }
+
+    /*
+     * plot a point on the graph relative to where the proton intersects with
+     * the measurePoint. eventually, changing the size and color of the dot
+     * depending on it's impact velocity, and the size of the dot depending
+     * on the location distribution (this will be a separate graph)
+     *
+     * Prof. Keller also mentioned that being able to export this data would
+     * be a nice-to-have
+     */
     void OnCollisionEnter(Collision collision)
     {
-        //GameObject point = Instantiate(dot, graph.transform); 
-        //point.SetActive(true);
         Debug.Log("quark relative velocity: " + collision.relativeVelocity);
         Debug.Log("quark contact: " + collision.GetContact(0).point);
+
+        Vector3 graphPoint = collision.GetContact(0).point;
+        GameObject point = Instantiate(dot, graphPoint, new Quaternion(), graph.transform);
+        point.SetActive(true);
     }
 }
