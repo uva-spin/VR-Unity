1e85ba9233dd6da50fe405966ba19f92ded5734a
Whoose-Joe <59741699+Whoose-Joe@users.noreply.github.com>
2022-10-18 22:52:18 -0400

    Re-added music and fixed FPS counter

--- Assets/Scripts/FPSCounter.cs
+++ Assets/Scripts/FPSCounter.cs
@@ -6,12 +6,15 @@ using TMPro;
 public class FPSCounter : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        InvokeRepeating("UpdateCounter", 0f, 0.1f);
-    }
 
-    void UpdateCounter() {
-        GetComponent<TMP_Text>().text = "FPS: " + Mathf.RoundToInt(Time.timeScale / Time.deltaTime);
+    float time = 0f;
+
+    void Update() {
+        time -= Time.timeScale;
+        if (time <= 0)
+        {
+            GetComponent<TMP_Text>().text = "FPS: " + Mathf.RoundToInt(Time.timeScale / Time.deltaTime);
+            time = 10f;
+        }
     }
 }
