3fc063b467fd777196996777445c184ef49c2e6c
Whoose-Joe <59741699+Whoose-Joe@users.noreply.github.com>
2023-01-03 17:36:43 -0500

    Please work

---
+++ Assets/Scripts/Quark.cs
@@ -0,0 +1,67 @@
+﻿using DG.Tweening;
+using System;
+using UnityEngine;
+
+public enum QuarkColor
+{
+    Red,
+    Green,
+    Blue,
+    GluonRed,
+    GluonGreen,
+    GluonBlue,
+    AntiRed,
+    AntiGreen,
+    AntiBlue
+}
+
+/// <summary>
+/// Set color and flavor of quark.
+/// </summary>
+public class Quark : TypeQuark
+{
+    [SerializeField]
+    private GameObject quarkSphere;
+
+    [SerializeField]
+    private GameObject quarkCore;
+
+    [SerializeField]
+    private GameObject quarkGlow;
+
+    public float Radius = 0.5f;
+
+    private ChangeOrbit changeOrbit;
+
+    public float f = 0.0f;
+
+    [SerializeField]
+    private AnimationCurve trailSizeCorrectionCurve;
+
+    public float GetEstimatedSpeed()
+    {
+        return changeOrbit.EstimatedSpeed;
+    }
+
+    private void Awake()
+    {
+        sphereMaterial = quarkSphere.GetComponent<Renderer>().material;
+        coreMaterial = quarkCore.GetComponent<Renderer>().material;
+        glowMaterial = quarkGlow.GetComponent<Renderer>().material;
+        trailRenderer = GetComponent<TrailRenderer>();
+
+        changeOrbit = GetComponent<ChangeOrbit>();
+
+        UpdateColor();
+    }
+
+    private void Update()
+    {
+        float tNormalized = Mathf.Clamp01(changeOrbit.CurrentOrbitRadius / changeOrbit.MaxRadius);
+        float coeff = trailSizeCorrectionCurve.Evaluate(tNormalized);
+
+        trailRenderer.time = trailTime * coeff;
+        trailRenderer.widthMultiplier = trailWidth;
+        if (forceLine) forceLine.widthMultiplier = trailWidth / 10f;
+    }
+}
