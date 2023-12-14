2e376ae65d688800f3b6189e3b6510d07327797b
Whoose-Joe <mycraft1001@gmail.com>
2023-12-14 11:29:23 -0500

    Reimported seaquarks into cartesion model. Also added random linear force to langragian, and random rotation of proton body to further add variation (visual only, does not impact actual calculations).

--- Assets/Scripts/Calculus/CartesianModel.cs
+++ Assets/Scripts/Calculus/CartesianModel.cs
@@ -1,23 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//Fancy way of saying "hey, if this is a built application and not the editor, don't use this"
+#if (UNITY_EDITOR && !UNITY_ANDROID)
+using UnityEditor;
+#endif
 
 public class CartesianModel : MonoBehaviour
 {
-    public int n = 2;
-    public int dimensions = 2;
-    public float R = 5;
+    public int n = 3;
+    public int dimensions = 3;
+    public float R = 0;
 
-    public float[] m = new float[3] { 1, 1, 2 };
-    public float Mc = 0.1f,
+    public float[] m = new float[3] { 2, 1, 1 };
+    public float Mc = 1e-05f,
     k = 5;
 
-    public Vector3[] x, x_dot, x_dot_dot, omega, omega_dot, A;
+    //Default values
+    public Vector3[] x = new Vector3[4] { new Vector3(0.5f, 0, 0), new Vector3(-0.25f, 0.4330127f, 0), new Vector3(-0.25f, -0.4330127f, 0), new Vector3(0, 0, 0) }, 
+        x_dot = new Vector3[4] { new Vector3(2, -2, 0), new Vector3(0, 4, -4), new Vector3(-4, 0, 4), new Vector3(0, 0, 0) }, 
+        x_dot_dot = new Vector3[4] { new Vector3(), new Vector3(), new Vector3(), new Vector3() }, 
+        omega = new Vector3[3] { new Vector3(), new Vector3(), new Vector3() }, 
+        omega_dot = new Vector3[3] { new Vector3(), new Vector3(), new Vector3() }, 
+        A = new Vector3[4] { new Vector3(0.5f, 0, -0.5f), new Vector3(-1, 1, 0), new Vector3(0, -1, 1), new Vector3(0, 0, 0) };
+    public float chaosFactor = 10, chaosShiftChance = 0.25f;
     public float Ra;
 
-    public float dt;
+    public float dt = 0.001f;
 
-    public float playbackSpeed = 0.01f;
+    public float playbackSpeed = 1f;
 
 
     public GameObject[] displays;
@@ -25,10 +36,17 @@ public class CartesianModel : MonoBehaviour
 
     public GameObject Point;
 
+    private Vector3 rotateProton = new Vector3();
+
+    //Initial values used for resetting without relaunching application
+    private Vector3[] _x, _x_dot, _x_dot_dot, _omega, _omega_dot, _A;
+
     private void Start()
     {
         if (!centerOfMass) centerOfMass = Instantiate(new GameObject()).transform;
         //FindObjectOfType<Pather>().active = true;
+        _x = x; _x_dot = x_dot; _x_dot_dot = x_dot_dot;
+        _omega = omega; _omega_dot = omega_dot; _A = A;
     }
 
     void Update()
@@ -63,8 +81,8 @@ public class CartesianModel : MonoBehaviour
 
             for (int i = 0; i < n; i++)
             {
-                displays[i].transform.position = x[i];
-                displays[i].transform.eulerAngles = omega[i];
+                displays[i].transform.localPosition = x[i];
+                //displays[i].transform.eulerAngles = omega[i];
 
                 //displays[i].transform.Rotate(Mathf.Cos(time), Mathf.Sin(time), 0, Space.Self);
                 if (Point)
@@ -104,7 +122,11 @@ public class CartesianModel : MonoBehaviour
             ang += Vector3.Cross(x_dot[i] - vel, x[i] - centerOfMass.position);
         }
 
-        Debug.Log(ang.magnitude);
+        if (Random.Range(0f, 1f) <= chaosShiftChance) ChangeRandomForce();
+
+        if (Random.Range(0f, 1f) <= .01) rotateProton = RandomForce() * 250;
+
+        transform.parent.Rotate(rotateProton * Time.deltaTime);
     }
 
     //Makes it so instead of having to refer to var[1224234325453] or something like that when inputting variables into the Lagrangian, you can just use x_dot.
@@ -138,6 +160,12 @@ public class CartesianModel : MonoBehaviour
             for (int i = 0; i < n; i++) x_dot_dot[3][j] += (k / Mc) * ((2 * R * (x[3][j] - x[i][j]) / Mathf.Sqrt(Sum(i, j))) - (x[3][j] - x[i][j]));
         }
 
+        //Randomness
+        for (int i = 0; i < n + 1; i++) {
+            x_dot_dot[i] += A[i] * chaosFactor;
+        }
+
+
         for (int j = 0; j < dimensions; j++) { for (int i = 0; i < n; i++) { output[3 * i + j] = x_dot[i][j]; } output[9 + j] = x_dot[3][j]; }
         for (int j = 0; j < dimensions; j++) { for (int i = 0; i < n; i++) { output[12 + 3 * i + j] = x_dot_dot[i][j]; } output[12 + 9 + j] = x_dot_dot[3][j]; }
         for (int j = 0; j < dimensions; j++) { for (int i = 0; i < n; i++) { output[36 + 3 * i + j] = omega_dot[i][j]; } }
@@ -153,4 +181,53 @@ public class CartesianModel : MonoBehaviour
         }
         return (output == 0) ? 0.00001f : output; //If the sum would return zero, return not zero (otherwise everything crashes due to a division by zero)
     }
+
+    public void ResetState() {
+        x = _x; x_dot = _x_dot; x_dot_dot = _x_dot_dot;
+        omega = _omega; omega_dot = _omega_dot; A = _A; 
+    }
+
+
+    private void ChangeRandomForce() {
+        int counterForceIndex = Random.Range(0, dimensions);
+        float mult = Random.Range(-1f, 1f);
+        int a = (counterForceIndex + 1) % dimensions,
+            b = (counterForceIndex + 2) % dimensions;
+        A[a] = RandomForce() / x_dot[a].magnitude;
+        A[b] = RandomForce() / x_dot[b].magnitude;
+        A[counterForceIndex] = -(A[a] * m[a] + A[b] * m[b]) / m[counterForceIndex];
+
+        A[0] *= mult;
+        A[1] *= mult;
+        A[2] *= mult;
+    }
+
+    private Vector3 RandomForce() {
+        return new Vector3(Random.Range(-0.5f, 0.5f), Random.Range(-0.5f, 0.5f), Random.Range(-0.5f, 0.5f));
+    }
 }
+
+#if (UNITY_EDITOR && !UNITY_ANDROID)
+[CustomEditor(typeof(CartesianModel))]
+public class SomeScriptEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        CartesianModel cartesianModel = (CartesianModel)target;
+        if (GUILayout.Button("Reset Proton"))
+        {
+            cartesianModel.ResetState();
+        }
+        EditorGUILayout.HelpBox("This is an implementation of the latest cartesion model in the overleaf document (plus some added randomness).\n" +
+                                "N stands for the number of quarks you want active (1-3).\n" +
+                                "Dimensions is the number of spatial dimensions you want active (1D, 2D, or 3D).\n" +
+                                "M and Mc are the masses of each quark and the flux tube's center.\n" +
+                                "K is the spring factor (how strong the strong force is).\n" +
+                                "X and X_dot are used for the positions and velocities of each quark and the flux tube.\n" +
+                                "A is the random force direction, and the chaos factor is how strong A is applied.\n" +
+                                "Omega, omega_dot, and Ra are all currently unused.\n" +
+                                "Dt is the level of precision each calculation is done by, and playback speed is how fast the simulation is run.", MessageType.Info);
+        DrawDefaultInspector();
+    }
+}
+#endif
