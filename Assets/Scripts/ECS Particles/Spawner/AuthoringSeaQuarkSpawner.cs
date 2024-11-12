09e739b8997e2d6fb1cae7fd806f4da059b8275d
Tron16 <63023214+Tron16@users.noreply.github.com>
2024-11-12 17:05:49 -0500

    test

--- Assets/Scripts/ECS Particles/Spawner/AuthoringSeaQuarkSpawner.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using Unity.Entities;
-
-public class AuthoringSeaQuarkSpawner : MonoBehaviour
-{
-    public GameObject prefab;
-}
--- Assets/Scripts/ECS Particles/Spawner/ComponentSeaQuarkSpawner.cs
+++
@@ -1,9 +0,0 @@
-using Unity.Entities;
-using Unity.Mathematics;
-
-public struct ComponentSeaQuarkSpawner : IComponentData
-{
-    public Entity prefab;
-    public float3 position;
-    public float charge;
-}
